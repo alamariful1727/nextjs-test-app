4f250e7130809a0c936e8b2f0a95b4c96f1a94d9
Ariful Alam <alamariful1727@gmail.com>
2021-01-03 22:17:08 +0600

    Typescript refactored and some clean-up

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+
+const Home = () => {
+  return (
+    <div>
+      <Head>
+        <title>Test Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>Home</h1>
+    </div>
+  );
+};
+
+export default Home;
