2319c3f439abc5171672ab3adffe6a85af8246ad
Ariful Alam <alamariful1727@gmail.com>
2021-01-08 21:52:19 +0600

    incremental site generation added successfully

--- pages/users/[id].tsx
+++ pages/users/[id].tsx
@@ -1,9 +1,10 @@
 import { GetStaticPaths, GetStaticProps } from 'next';
 import Head from 'next/head';
+import DefaultErrorPage from 'next/error';
 import { useRouter } from 'next/router';
 import { ParsedUrlQuery } from 'querystring';
 import { User } from '../../types';
-import { App_Title } from '../../utils';
+import { App_Title, isEmpty } from '../../utils';
 
 type Props = {
   user: User;
@@ -13,7 +14,7 @@ interface Params extends ParsedUrlQuery {
   id: string;
 }
 
-const UserProfile = ({ user: { id, name, email, username } }: Props) => {
+const UserProfile = ({ user }: Props) => {
   const router = useRouter();
 
   //? If the page is not yet generated, this will be displayed
@@ -22,22 +23,34 @@ const UserProfile = ({ user: { id, name, email, username } }: Props) => {
     return <div>Loading...</div>;
   }
 
+  // This includes setting the noindex header because static files always return a status 200 but the rendered not found page page should obviously not be indexed
+  if (!user || isEmpty(user)) {
+    return (
+      <>
+        <Head>
+          <meta name="robots" content="noindex" />
+        </Head>
+        <DefaultErrorPage statusCode={404} />
+      </>
+    );
+  }
+
   return (
     <div>
       <Head>
         <title>
-          {App_Title} | {id}
+          {App_Title} | {user.id}
         </title>
       </Head>
       <div className="border border-yellow-500 bg-gray-800 rounded-md text-white py-5 px-2 mb-5">
         <h2>
-          <span className="font-bold">Name</span> : {name}
+          <span className="font-bold">Name</span> : {user.name}
         </h2>
         <h2>
-          <span className="font-bold">Email</span> : {email}
+          <span className="font-bold">Email</span> : {user.email}
         </h2>
         <h2>
-          <span className="font-bold">Username</span> : {username}
+          <span className="font-bold">Username</span> : {user.username}
         </h2>
       </div>
     </div>
@@ -45,17 +58,23 @@ const UserProfile = ({ user: { id, name, email, username } }: Props) => {
 };
 
 export const getStaticProps: GetStaticProps<Props, Params> = async (ctx) => {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/users/${ctx.params?.id}`);
-  const user: User = await res.json();
+  try {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/users/${ctx.params?.id}`);
+    const user: User = await res.json();
 
-  return {
-    props: {
-      user,
-    },
-    //? Re-generate the post at most once per 5 second
-    //? if a request comes in
-    revalidate: 1,
-  };
+    return {
+      props: {
+        user,
+      },
+      //? Re-generate the post at most once per 5 second
+      //? if a request comes in
+      revalidate: 1,
+    };
+  } catch (error) {
+    return {
+      notFound: true,
+    };
+  }
 };
 
 export const getStaticPaths: GetStaticPaths = async (ctx) => {
